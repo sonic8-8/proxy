a40f1ddb7c797f0ed9b7687bfb66e9e5eb0be6d2
이승환 <seaung13@gmail.com>
2025-03-08 08:13:45 +0900

    test: JDK 동적 프록시 - 예제 추가

--- src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
+++ src/test/java/hello/proxy/jdkdynamic/ReflectionTest.java
@@ -3,6 +3,7 @@ package hello.proxy.jdkdynamic;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 @Slf4j
@@ -39,32 +40,37 @@ public class ReflectionTest {
 
     @Test
     void reflection1() throws Exception {
-        Class<?> classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest");
+        Class<?> classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
 
         Hello target = new Hello();
-        //callA 메서드 정보
+        // callA 메서드 정보
         Method methodCallA = classHello.getMethod("callA");
         Object result1 = methodCallA.invoke(target);
-        log.info("result1={}", result1);
-        //callB 메서드 정보
+        log.info("result2={}", result1);
+
+        // callB 메서드 정보
         Method methodCallB = classHello.getMethod("callB");
         Object result2 = methodCallB.invoke(target);
-        log.info("result2={}", result2);
+        log.info("result1={}", result2);
     }
 
     @Test
     void reflection2() throws Exception {
-        Class classHello =
-                Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+        Class<?> classHello = Class.forName("hello.proxy.jdkdynamic.ReflectionTest$Hello");
+
         Hello target = new Hello();
         Method methodCallA = classHello.getMethod("callA");
         dynamicCall(methodCallA, target);
+
         Method methodCallB = classHello.getMethod("callB");
         dynamicCall(methodCallB, target);
     }
-    private void dynamicCall(Method method, Object target) throws Exception {
+
+    private void dynamicCall(Method methodCallA, Hello target) throws Exception {
         log.info("start");
-        Object result = method.invoke(target);
+        Object result = methodCallA.invoke(target);
         log.info("result={}", result);
     }
+
+
 }
